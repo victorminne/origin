fdeaf1d2f6bb40f650841a2c7d0f5d24767ab0a9
Victor Minne <3aminne.v@gmail.com>
2020-10-01 11:02:57 +0200

    début des onclicks
    start des onclicks juste un debut ici, juste de quoi récupérer les id.

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 // ce fichier va servir pour les différentes fonction, 
 //cela permettra de d'avoir des pages interactives et un plus petit nombre de fichier au sein du projet
+
+var d = document.getElementById("d");
+var d_s = document.getElementById("d_s");
+var d_f = document.getElementById("d_f");
+var d_g = document.getElementById("d_g");
+var d_m = document.getElementById("d_m");
+
+d.onclick = function()
+
+
+function link() {
+    
+}
