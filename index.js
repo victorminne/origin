5fcaf04f9973e241b4f59cf686e1f545dcfd6400
Victor Minne <3aminne.v@gmail.com>
2020-10-07 10:01:04 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 // ce fichier va servir pour les différentes fonction, 
 //cela permettra de d'avoir des pages interactives et un plus petit nombre de fichier au sein du projet
 
+function link() {
+    // fonction permettant de filtrer la recherche en fonction du lien cliqué
+}
+
 var d = document.getElementById("d");
 var d_s = document.getElementById("d_s");
 var d_f = document.getElementById("d_f");
@@ -12,9 +16,3 @@ d_s.onclick = link(d_s);
 d_f.onclick = link(d_f);
 d_g.onclick = link(d_g);
 d_m.onclick = link(d_g);
-
-
-
-function link() {
-    
-}
